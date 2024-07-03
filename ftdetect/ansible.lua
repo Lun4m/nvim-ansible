bde04f5ca5e442352e56b9ec78bb727db1a23eb1
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-07-03 11:55:45 +0200

    Update ansible.lua

--- ftdetect/ansible.lua
+++ ftdetect/ansible.lua
@@ -1,7 +1,7 @@
 if vim.filetype then
   vim.filetype.add({
     pattern = {
-      [".*/anisble/.*%.ya?ml"] = "yaml.ansible",
+      [".*/ansible/.*%.ya?ml"] = "yaml.ansible",
       [".*/host_vars/.*%.ya?ml"] = "yaml.ansible",
       [".*/group_vars/.*%.ya?ml"] = "yaml.ansible",
       [".*/group_vars/.*/.*%.ya?ml"] = "yaml.ansible",
@@ -13,8 +13,8 @@ if vim.filetype then
 else
   vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     pattern = {
-      "*/anisble/*.yml",
-      "*/anisble/*.yaml",
+      "*/ansible/*.yml",
+      "*/ansible/*.yaml",
       "*/host_vars/*.yml",
       "*/host_vars/*.yaml",
       "*/group_vars/*.yml",
